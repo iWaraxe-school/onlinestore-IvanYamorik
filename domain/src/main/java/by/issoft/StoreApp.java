9e5fe1991987796799a25b2198f68c56dc4dc80d
IvanYamorik <i.v.yamorik@gmail.com>
2022-02-14 00:25:56 +0300

    oop-1try

--- domain/src/main/java/by/issoft/StoreApp.java
+++ domain/src/main/java/by/issoft/StoreApp.java
@@ -3,6 +3,9 @@ package by.issoft;
 public class StoreApp {
     public static void main(String[] args) {
 
-        Product fromDB = new Product(id: 1, name: "Milk1", rate: 1, price: 4);
+        Product fromDB = new Product(id: 1, name: "Milk1", rate: 1, price: 4, milkName: "1.5%");
+        Product fromDB = new Product(id: 2, name: "Milk2", rate: 1, price: 4.5, milkName: "3.6%");
+        Product fromDB = new Product(id: 3, name: "T1000", rate: 1, price: 4, bikeName: "Harley");
+        Product fromDB = new Product(id: 4, name: "S22", rate: 1, price: 4, phoneName: "Samsung");
     }
 }
