2b14143b1017d20f79b97485b2543cc8fab01158
IvanYamorik <i.v.yamorik@gmail.com>
2022-03-22 10:26:47 +0300

    added reflection

--- consoleApp/src/main/java/by/issoft/storeapp/StoreApp.java
+++ consoleApp/src/main/java/by/issoft/storeapp/StoreApp.java
@@ -4,8 +4,12 @@ package by.issoft.storeapp;
 import by.issoft.store.Store;
 import by.issoft.store.StoreFiller;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class StoreApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
 
         System.out.println(" My store: ");
 
@@ -17,5 +21,38 @@ public class StoreApp {
         String storeString = newStore.toString();
 
         System.out.println(storeString);
+        //reflections
+        Class<?> storeClass = Class.forName("by.issoft.store.Store");
+        Constructor<?> storeConstructor = storeClass.getConstructor();
+        Store newStore2 = (Store) storeConstructor.newInstance();
+
+        Class<?> storeFillerClass = Class.forName("by.issoft.store.StoreFiller");
+        Constructor<?> storeFillerConstructor = storeFillerClass.getConstructor(Store.class);
+        StoreFiller storeFiller2 = (StoreFiller) storeFillerConstructor.newInstance(newStore2);
+
+        //Method.fillStoreRandomly
+        Method fillStoreRandomlyMethod = storeFillerClass.getDeclaredMethod("fillStoreRandomly");
+        fillStoreRandomlyMethod.invoke(storeFiller2);
+
+        Method toStringMethod = storeClass.getDeclaredMethod("toString");
+        String storeString2 = (String) toStringMethod.invoke(newStore2);
+
+        System.out.println(" My store with reflection: ");
+        System.out.println(storeString2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
