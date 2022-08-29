52fe6dd3acbd29cbb7c9a40f36f4df285ed98011
IvanYamorik <i.v.yamorik@gmail.com>
2022-08-29 23:24:18 +0300

    http server

---
+++ store/src/main/java/by/issoft/store/HttpFiller.java
@@ -0,0 +1,12 @@
+package by.issoft.store;
+
+import java.sql.SQLException;
+
+public class HttpFiller implements StoreFiller {
+    Store store;
+
+    @Override
+    public void fillStoreRandomly() throws SQLException {
+        
+    }
+}
